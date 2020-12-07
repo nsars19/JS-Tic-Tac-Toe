6165d5fb61f7c94b85b9802a0df4d1b14e6f2cfc
Nick <nsardonini@gmail.com>
2020-12-07 16:46:41 -0500

    add functions to check for win conditions

--- main.js
+++ main.js
@@ -25,11 +25,48 @@ const gameBoard = (() => {
     return !board.flat().some(tile => tile === "")
   }
 
+  const _tripleMatch = line => { return (line == "XXX" || line == "OOO") ? true : false }
+
+  const _horizontalWin = () => {
+    for (let i = 0; i < 3; i++) {
+      let current = board[i].join("")
+      if (_tripleMatch(current)) {
+        return true;
+      }
+    }
+    return false
+  }
+
+  const _verticalWin = () => {
+    for (let i = 0; i < 3; i++) {
+      let current = ""
+      for (let j = 0; j < 3; j++) {
+        current += board[j][i]
+      }
+      if (_tripleMatch(current)) {
+        return true
+      }
+    }
+    return false
+  }
+
+  const _diagonalWin = () => {
+    firstDiagonal = board[0][0] + board[1][1] + board[2][2]
+    secondDiagonal = board[0][2] + board[1][1] + board[2][0]
+
+    return _tripleMatch(firstDiagonal) ? true : _tripleMatch(secondDiagonal) ? true : false
+  }
+
+  const isWin = () => {
+    return (_horizontalWin() || _verticalWin() || _diagonalWin()) ? true : false
+  }
+  
   return {
     getBoard,
     clearBoard,
     addToBoard,
     boardFull,
+    isWin,
   }
 })()
 
